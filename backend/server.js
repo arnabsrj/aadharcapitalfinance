6061f7ed1af2226e01e646b18099d494f4587520
Shadab khan <srjglobal.shadab.javadeveloper@gmail.com>
2025-12-05 17:06:46 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,15 @@ console.log("=== SERVER FILE STARTED ===");
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173", // your local frontend port (Vite)
+    "https://aadharcapitalfinance.vercel.app"
+  ],
+  credentials: true,
+}));
+
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
